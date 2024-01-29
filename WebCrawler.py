4a05eaacead7831ed0a45945e9b3fcc86f655c55
w455up <104774553+w455up@users.noreply.github.com>
2024-01-30 00:12:54 +0800

    Update WebCrawler.py

--- WebCrawler.py
+++ WebCrawler.py
@@ -3,7 +3,7 @@ from datetime import datetime, time as dt_time
 import openpyxl
 import requests
 from bs4 import BeautifulSoup
-
+import pytz
 def job():
     wb = openpyxl.load_workbook('test.xlsx')
     ws = wb.active
@@ -13,10 +13,10 @@ def job():
     soup = BeautifulSoup(web.text, "html.parser")
     number = soup.find_all('span', class_='number-current')
     peo = number[1].text
-    current_time = datetime.now().strftime('%m-%d')
+    current_time = datetime.now(pytz.timezone('Asia/Taipei')).strftime('%m-%d')
     date = datetime.now().strftime('%H:%M')
     weekday = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
-    weekofday = weekday[datetime.now().weekday()]
+    weekofday = weekday[datetime.now(pytz.timezone('Asia/Taipei')).weekday()]
 
     url2 = 'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0001-001?Authorization=CWA-AC5922D2-E03C-4C0D-9818-42B17D48DF87&downloadType=WEB&format=JSON'
     data = requests.get(url2)
@@ -38,6 +38,6 @@ def job():
 
     ws.append([peo, date, current_time, weekofday, dictionary['新北市']['淡水觀海']])
     wb.save('test.xlsx') 
-    print(f"已執行一次任務{datetime.now().strftime('%H:%M')}")
+    print(f"已執行一次任務{datetime.now(pytz.timezone('Asia/Taipei')).strftime('%H:%M')}")
 
 job()
